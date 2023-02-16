cda0a23885d778bc21ec2a9c74a574f1257abdf2
SafiyaNawar <114554826+SafiyaNawar@users.noreply.github.com>
2023-02-16 09:54:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, NEW World\n";
   cout << "Hello I have made a change, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
