65a80ff8a7f1bd96bda665a364fb5da6196ba8f8
SafiyaNawar <114554826+SafiyaNawar@users.noreply.github.com>
2023-02-16 10:14:45 +0530

    Create task5file.cpp

---
+++ task5file.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, from tsak 5\n";
+  cout << "I have successfully made a cpp file\n";
+  return 0;
+}
